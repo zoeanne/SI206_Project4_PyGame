a48a658f574c68b4400d749a05b21d7bd1e6a5a6
Zoe <zoeanne@umich.edu>
2016-12-01 01:58:28 -0500

    added gameover function and music

--- icecream.py
+++ icecream.py
@@ -1,28 +1,29 @@
-from pygame import *
+import pygame
 from pygame.sprite import *
 import random
 import sys
+import time
 
-init()
+pygame.init()
 
 width = 800
 height = 600
 
-screen = display.set_mode((width, height))
-display.set_caption("window title")
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("window title")
 clock = pygame.time.Clock()
 
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 
-f = font.Font(None, 25)
+f = pygame.font.Font(None, 40)
 
 class Tub(Sprite):   
     def __init__(self):
         Sprite.__init__(self)
-        self.image = image.load("tub.bmp").convert_alpha()
-        self.image = transform.scale(self.image, (100, 100))
+        self.image = pygame.image.load("tub.bmp").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
         self.rect.x = width/2
         self.rect.y = height - 100       
@@ -37,8 +38,8 @@ class Tub(Sprite):
 class Single_scoop(Sprite):
     def __init__(self, y_pos):
         Sprite.__init__(self)
-        self.image = image.load("singlescoop_pink.bmp").convert_alpha()
-        self.image = transform.scale(self.image, (100, 100))
+        self.image = pygame.image.load("singlescoop_pink.bmp").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, width-100) #random x position, minus 100 so image does not go off screen
         self.rect.y = -9000 + y_pos #start off screen so starting point to fall down is not visible
@@ -51,8 +52,8 @@ class Single_scoop(Sprite):
 class Triple_scoop(Single_scoop):
     def __init__(self, y_pos):
         Sprite.__init__(self)
-        self.image = image.load("triplescoop.bmp").convert_alpha()
-        self.image = transform.scale(self.image, (100, 100))
+        self.image = pygame.image.load("triplescoop.bmp").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, width-100)
         self.rect.y = -9000 + y_pos
@@ -60,11 +61,21 @@ class Triple_scoop(Single_scoop):
 class Scooper(Single_scoop):
     def __init__(self, y_pos):
         Sprite.__init__(self)
-        self.image = image.load("scooper.bmp").convert_alpha()
-        self.image = transform.scale(self.image, (100, 100))
+        self.image = pygame.image.load("scooper.bmp").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, width-100)
         self.rect.y = -9000 + y_pos
+
+
+def gameover():
+    f = pygame.font.Font(None, 30)
+    t = f.render("Game Over! Your ending score is " + str(score), False, red)
+    screen.blit(t, (200,300)) #x, y 
+    pygame.display.update()
+    time.sleep(5)
+    pygame.quit()
+    sys.exit()
         
 tub = Tub()
 blocks = []
@@ -79,17 +90,21 @@ for i in range(20):
 for i in range(30):
     y = i * 250 
     enemy_blocks.append(pygame.sprite.RenderPlain(Scooper(y)))
+
 sprites = pygame.sprite.RenderPlain(tub, *blocks, *extra_blocks, *enemy_blocks)
 score = 0
-#maxscore user can reach is 120 points 
+#maxscore user can reach is 120 points
 
 def game_loop():
-    
+
+    pygame.mixer.music.load("Ice_Cream_Song_Loop.wav")
+    pygame.mixer.music.play(-1, 0.0)
+
     gameExit = False
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                quit()
+                pygame.quit()
                 sys.exit()
                 
         global score
@@ -98,8 +113,7 @@ def game_loop():
         for i in extra_blocks:
             score += (3*(len(pygame.sprite.spritecollide(tub, i, True))))
         for i in enemy_blocks:
-            score -= (1*(len(pygame.sprite.spritecollide(tub, i, True))))
-
+            score -= (3*(len(pygame.sprite.spritecollide(tub, i, True))))
             
         sprites.update()
         screen.fill(white)
@@ -107,8 +121,11 @@ def game_loop():
         screen.blit(t, (0,0))
         sprites.update()
         sprites.draw(screen)
-        display.update()
-        clock.tick(60)
+        pygame.display.update()
+##        clock.tick(60)
+
+        if len(sprites) == 1:
+            gameover()
 
         
 game_loop()
