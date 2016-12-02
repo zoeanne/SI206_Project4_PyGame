6c19055c8adf309bcb1bda9fc6ae93272f887724
Zoe <zoeanne@umich.edu>
2016-12-01 23:56:58 -0500

    added game_intro, looping, and Truck class

--- icecream.py
+++ icecream.py
@@ -9,13 +9,17 @@ pygame.init()
 width = 800
 height = 600
 
+score = 0
+
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("window title")
+pygame.display.set_caption("Super Scooping Challenge")
 clock = pygame.time.Clock()
 
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
+pink = (255,20,147)
+blue = (240,248,255)
 
 f = pygame.font.Font("freesansbold.ttf", 30)
 
@@ -35,6 +39,25 @@ class Tub(Sprite):
         if keys [pygame.K_RIGHT]:
             self.rect.x += dist
 
+class Truck(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pygame.image.load("truck.bmp").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = height - 100      
+    def update(self):
+        dist = 1
+        self.rect.x += dist
+        if self.rect.x > width:
+            self.kill()
+
+
+
+
+        
+
 class Single_scoop(Sprite):
     def __init__(self, y_pos):
         Sprite.__init__(self)
@@ -43,7 +66,6 @@ class Single_scoop(Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, width-100) #random x position, minus 100 so image does not go off screen
         self.rect.y = -9000 + y_pos #start off screen so starting point to fall down is not visible
-##        print(self.rect.y)
     def update(self):
         self.rect.y += 3
         if self.rect.y > height:
@@ -67,51 +89,123 @@ class Scooper(Single_scoop):
         self.rect.x = random.randint(0, width-100)
         self.rect.y = -9000 + y_pos
 
-def gameover():
-    a = pygame.font.Font("freesansbold.ttf", 27)
-    b = pygame.font.Font(None, 25)  
-    if score < 50:
-        x = a.render("Novice Scooper! Your scooping score is " + str(score), False, red)
-        y = b.render("Keep practicing to become an EXPERT Super Scooper! (window will close automatically)", False, black)
-    elif score >=50 and score <80:
-        x = a.render("Super Scooper! Your scooping score is " + str(score), False, red)
-        y = b.render("Keep practicing to become an EXPERT Super Scooper! (window will close automatically)", False, black)
-    elif score >=80:
-        x = a.render("EXPERT Super Scooper! Your scooping score is " + str(score), False, red)
-        y = b.render("You've demonstrated your skills and are now reading to show the world", False, black)
-        z = b.render("your Super Scooping abilities! Thanks for playing! (window will close automatically)", False, black)
-        screen.blit(z, (50, 350))
-    screen.blit(x, (50,200)) #x, y
-    screen.blit(y, (50, 300))
-    pygame.display.update()
-    time.sleep(10)
-    pygame.quit()
-    sys.exit()
-        
-tub = Tub()
-blocks = []
-extra_blocks = []
-enemy_blocks = []
-for i in range(60):
-    y = i * 150
-    blocks.append(pygame.sprite.RenderPlain(Single_scoop(y)))
-for i in range(20):
-    y = i * 350
-    extra_blocks.append(pygame.sprite.RenderPlain(Triple_scoop(y)))
-for i in range(30):
-    y = i * 250 
-    enemy_blocks.append(pygame.sprite.RenderPlain(Scooper(y)))
 
-sprites = pygame.sprite.RenderPlain(tub, *blocks, *extra_blocks, *enemy_blocks)
-score = 0
-#maxscore user can reach is 120 points
 
-def game_loop():
+
+
+def game_intro():
 
     pygame.mouse.set_visible(False)
     
     pygame.mixer.music.load("Ice_Cream_Song_Loop.wav")
     pygame.mixer.music.play(-1, 0.0)
+
+    truck = Truck()
+    sprites = pygame.sprite.RenderPlain(truck)
+
+    intro = True
+    
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game_loop()
+                    
+        screen.fill(blue)              
+        x = pygame.font.Font("freesansbold.ttf", 40)
+        y = pygame.font.Font("freesansbold.ttf",30)
+        z = pygame.font.Font("freesansbold.ttf",20)
+        
+        a = x.render("Super Scooping Challenge!", False, pink)
+        b = y.render("Press the spacebar to start", False, pink)
+        c = z.render("Instructions: Use the arrow keys to catch as much ice cream as you can,", False, black)
+        d = z.render("but avoid the scooper!", False, black)
+        e = z.render("Single scoop = +1", False, black)
+        f = z.render("Triple scoop = +3", False, black)
+        g = z.render("Scooper = -3", False, black)
+        screen.blit(a, (150,100))
+        screen.blit(b, (200,150))
+        screen.blit(c, (50, 220))
+        screen.blit(d, (50, 250))
+        screen.blit(e, (50, 300))
+        screen.blit(f, (50, 330))
+        screen.blit(g, (50, 360))
+        sprites.update()
+        sprites.update()
+        sprites.draw(screen)
+        pygame.display.update()
+
+
+
+
+def gameover():
+    a = pygame.font.Font("freesansbold.ttf", 27)
+    b = pygame.font.Font(None, 25)
+    global score
+    if score < 50:
+        x = a.render("Novice Scooper! Your scooping score is " + str(score), False, red)
+        y = b.render("Keep practicing to become an EXPERT Super Scooper! (press spacebar to play again)", False, black)
+    if score >=50 and score <80:
+        x = a.render("Super Scooper! Your scooping score is " + str(score), False, red)
+        y = b.render("Keep practicing to become an EXPERT Super Scooper! (press spacebar to play again)", False, black)
+    if score >=80:
+        x = a.render("EXPERT Super Scooper! Your scooping score is " + str(score), False, red)
+        y = b.render("You've demonstrated your skills and are now reading to show the world", False, black)
+        z = b.render("your Super Scooping abilities! Thanks for playing! (press spacebar to play again)", False, black)
+        screen.blit(z, (50, 350))
+    screen.blit(x, (50,200)) #x, y
+    screen.blit(y, (50, 300))
+    pygame.display.update()
+
+    gameover = True
+
+    while gameover:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    score = 0
+                    game_loop()
+
+            pygame.display.update()
+                    
+
+               
+        
+
+#maxscore user can reach is 120 points
+
+def game_loop():
+
+##    bg = pygame.image.load("bg.bmp").convert()
+##    screen.blit(bg, (0,0))
+
+    pygame.mouse.set_visible(False)
+
+
+    tub = Tub()
+    blocks = []
+    extra_blocks = []
+    enemy_blocks = []
+    for i in range(60):
+        y = i * 150
+        blocks.append(pygame.sprite.RenderPlain(Single_scoop(y)))
+    for i in range(20):
+        y = i * 350
+        extra_blocks.append(pygame.sprite.RenderPlain(Triple_scoop(y)))
+    for i in range(30):
+        y = i * 250 
+        enemy_blocks.append(pygame.sprite.RenderPlain(Scooper(y)))
+
+    sprites = pygame.sprite.RenderPlain(tub, *blocks, *extra_blocks, *enemy_blocks)
+
 
     gameExit = False
     while not gameExit:
@@ -140,6 +234,8 @@ def game_loop():
         if len(sprites) == 1:
             gameover()
 
-        
-game_loop()
+
+game_intro()        
+
+
 
